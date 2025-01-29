f7ec0cd4c0c70f123159d2324b45655462a47429
fujiao <1033932795@qq.com>
2025-01-29 03:56:59 +0000

    check Phishpedia

--- 11_code/reproduce_phishpedia/eval_phishpedia.py
+++ 11_code/reproduce_phishpedia/eval_phishpedia.py
@@ -76,7 +76,6 @@ if __name__ == '__main__':
     print(f"Start Eval Time: {startTime}")
     parser = argparse.ArgumentParser()
     parser.add_argument('-f', "--input_csv",
-                        # default="aa.csv",
                         default="data_test/data_test.csv",
                         help='Input csv path to test')
     
@@ -87,6 +86,31 @@ if __name__ == '__main__':
     parser.add_argument('--no_repeat', action='store_true')
 
     args = parser.parse_args()
+
+    
+    # 22
+    # ELE_CFG_PATH = configs['ELE_MODEL']['CFG_PATH']
+    # ELE_WEIGHTS_PATH = configs['ELE_MODEL']['WEIGHTS_PATH']
+    # ELE_CONFIG_THRE = configs['ELE_MODEL']['DETECT_THRE']
+    # ELE_MODEL = config_rcnn(ELE_CFG_PATH, ELE_WEIGHTS_PATH, conf_threshold=ELE_CONFIG_THRE)
+    # SIAMESE_THRE, SIAMESE_MODEL, LOGO_FEATS, LOGO_FILES = None, None, None, None
+    # # siamese model
+    # SIAMESE_THRE = configs['SIAMESE_MODEL']['MATCH_THRE']
+    
+    # SIAMESE_MODEL, LOGO_FEATS, LOGO_FILES = phishpedia_config(
+    #     num_classes=configs['SIAMESE_MODEL']['NUM_CLASSES'],
+    #     weights_path=configs['SIAMESE_MODEL']['WEIGHTS_PATH'],
+    #     targetlist_path=configs['SIAMESE_MODEL']['TARGETLIST_PATH'])
+    # print('Finish loading protected logo list')
+    # print("feature save to ", os.path.join(os.path.dirname(__file__), 'LOGO_FEATS'))
+    # np.save(os.path.join(os.path.dirname(__file__), 'LOGO_FEATS'), LOGO_FEATS)
+    # np.save(os.path.join(os.path.dirname(__file__), 'LOGO_FILES'), LOGO_FILES)
+    # DOMAIN_MAP_PATH = configs['SIAMESE_MODEL']['DOMAIN_MAP_PATH']
+
+
+
+
+
 
     ELE_MODEL, SIAMESE_THRE, SIAMESE_MODEL, LOGO_FEATS, LOGO_FILES, DOMAIN_MAP_PATH = load_config(cfg_path="configs.yaml", reload_targetlist=True)
     # print(ELE_MODEL, SIAMESE_THRE, SIAMESE_MODEL, LOGO_FEATS, LOGO_FILES, DOMAIN_MAP_PATH)
